ce8b1bb852a96af8711dc909cf5817c89fbc04a8
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-24 19:44:04 +0800

    Business Case 3 h/w - Simple/Naive Encryption Function
    Monday 24 Aug 2015 5:45pm GMT+8
    
    Business Case 3: Implementing a simple/naive encryption function
    
    * The IT manager of Dafesty wants to create a simple encryption function
    for use in various situations inside the application.
    * Write a static method: Encrypt(string s) that would return the
    encrypted value.  The purpose of this method is to encrypt the string s.
    You may follow a simple, naive algorithm for encryption as described
    below.  String s can be only alpha numeric (no special characters
    permitted)
    
    NOTE: Steps for Encryption below:
    
    Step 1:
    Change each character in the string by replacing it with the next
    character in the alphabet series.  For instance, letter a would be
    replaced by letter b, letter p would be replaced by letter q etc. and
    letter z would be replaced by a.  Similarly letter A would be replaced
    by letter B, letter P by letter Q and letter Z by A.  Likewise,
    character 1 would be replaced by character 2, character 5 by character 6
    and character 9 by character 0.
    
    Step 2:
    Invert the word by making a mirror image of it.
    
    Example:
    Consider the word:            SeazAme94
    FirstTransformation:        TfbaBnf05
    Second Transformation: 50fnBabfT
    
    Mon 24 Aug 2015 7:43pm GMT+8

---
+++ BusinessCase3/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+/*
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+namespace BusinessCase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Business Case 3 - Implementing a simple/naive encryption function");
+
+            string s;
+
+            Console.WriteLine("Please type in a string to be encrypted using the AlphaNumeric Characters only, i.e. a-z, A-Z & 0-9: ");
+            s = GetCleanUserInput();
+
+            Console.WriteLine("\n\nAfter Encryption:\n\n" + Encrypt(s) + "\n");
+        }
+
+        static string GetCleanUserInput()
+        {
+            int i, curr_i;
+            string strTemp = "";
+            Boolean InputClean = true, CheckedNotOK = true;
+
+            do
+            {
+                try
+                {
+                    strTemp = "";
+                    strTemp = Convert.ToString(Console.ReadLine());
+
+                    i = 0;
+                    InputClean = true;
+
+                    while((i < strTemp.Length) && InputClean)
+                    {
+                        curr_i = GetASCIICode(strTemp[i]);
+
+                        // a = 97
+                        // z = 122
+                        // A = 65
+                        // Z = 90
+                        // 0 = 48
+                        // 9 = 57
+
+                        if ((curr_i >= 97 && curr_i <= 122) || (curr_i >= 65 && curr_i <= 90) || (curr_i >= 48 && curr_i <= 57))
+                            InputClean = true;
+                        else
+                        {
+                            InputClean = false;
+                        }
+
+                        i++;
+                    }
+
+                    if (InputClean)
+                        CheckedNotOK = false;
+                    else
+                    {
+                        CheckedNotOK = true;
+                        Console.WriteLine("Input unacceptable. Please try again: ");
+                    }
+
+                }
+                catch
+                {
+                    CheckedNotOK = true;
+                }
+
+            } while (CheckedNotOK);
+
+
+            return strTemp;
+        }
+
+        static int GetASCIICode(char currChar)
+        {
+            return (int)currChar;
+        }
+
+        static string Encrypt(string s)
+        {
+            string strFirstTrans, strSecondTrans;
+
+            strFirstTrans = FirstTransformation(s);
+
+            strSecondTrans = SecondTransformation(strFirstTrans);
+
+//            return strFirstTrans;
+            return strSecondTrans;
+        }
+
+        static string FirstTransformation(string s)
+        { // a > b, p > q, A > Z, P > Q, z > a, Z > A, 1 > 2, 9 > 0
+
+            string strTemp = "";
+            int key = 1; // i.e. move the ASCII code one position fwd (greater in value)
+
+            int[] intArrASCIICodes = new int[s.Length];
+            int[] intArrTranslatedCode = new int[s.Length];
+
+            intArrASCIICodes = GetASCIINumbers(s);
+
+            intArrTranslatedCode = TranslateASCIICode(intArrASCIICodes, key);
+
+            strTemp = TranslateBackToCharacters(intArrTranslatedCode);
+
+            return strTemp;
+        }
+
+        static int[] GetASCIINumbers(string s)
+        {
+            // a = 97
+            // z = 122
+            // A = 65
+            // Z = 90
+            // 0 = 48
+            // 9 = 57
+
+            int[] intArrS = new int[s.Length];
+            int i;
+
+            for (i = 0; i < s.Length; i++)
+            {
+                intArrS[i] = (int)s[i];
+            }
+
+            return intArrS;
+        }
+
+        static int[] TranslateASCIICode(int[] intArrASCIICode, int key)
+        {
+            int[] intArrTranslatedCode = new int[intArrASCIICode.Length];
+            int i;
+
+            for (i = 0; i < intArrASCIICode.Length; i++)
+            {
+                switch (intArrASCIICode[i])
+                {
+                    case  57: intArrTranslatedCode[i] = (48 - 1) + key; break; // case '9' > ('0' - 1)
+                    case  90: intArrTranslatedCode[i] = (65 - 1) + key; break; // case 'Z' > ('A' - 1)
+                    case 122: intArrTranslatedCode[i] = (97 - 1) + key; break; // case 'z' > ('a' - 1)
+                    default : intArrTranslatedCode[i] = intArrASCIICode[i] + key; break;
+                }
+            }
+
+            return intArrTranslatedCode;
+        }
+
+        static string TranslateBackToCharacters(int[] intArrTranslatedASCIICode)
+        {
+            string strTemp = "";
+            int i;
+
+            for (i = 0; i < intArrTranslatedASCIICode.Length; i++)
+            {
+                strTemp = strTemp + (char)intArrTranslatedASCIICode[i];
+            }
+
+            return strTemp;
+        }
+
+        static string SecondTransformation(string s)
+        {
+            string strTemp = "";
+            int i;
+
+            for (i = (s.Length - 1); i >= 0; i--)
+            {
+                strTemp = strTemp + s[i];
+            }
+
+            return strTemp;
+        }
+    }
+}
